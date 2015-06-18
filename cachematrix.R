7e6abd64181f961a07704523f976e60ebf5fa813
clpong <clpong@hotmail.com>
2015-06-18 10:27:42 +0800

    modify the r file

--- cachematrix.R
+++ cachematrix.R
@@ -27,18 +27,6 @@ makeCacheMatrix <- function(x = matrix()) {
              getinverse = getinverse)
 }
 
-#makeCacheMatrix <- function(x = matrix()) {
-#        inv <- NULL
-#        set <- function(y) {
-#                x <<- y
-#                inv <<- NULL
-#        }
-#     get <- function() x
-#        setinverse <- function(inverse) inv <<- inverse
-#        getinverse <- function() inv
-#        list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
-#}
-
 ## The following function calculates the inverse of the special "matrix" created with the above function. 
 ## However, it first checks to see if the inverse has already been calculated. 
 ## If so, it gets the inverse from the cache and skips the computation. 
